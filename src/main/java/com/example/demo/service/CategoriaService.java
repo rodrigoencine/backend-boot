9a818ecf3a9f6febc487b64020f5c24c49b1ddc1
Rodrigo Encine <rodrigoencine@hotmail.com>
2020-05-16 19:49:04 -0300

    Inserindo uma nova categoria

--- src/main/java/com/example/demo/service/CategoriaService.java
+++ src/main/java/com/example/demo/service/CategoriaService.java
@@ -21,4 +21,9 @@ public class CategoriaService {
 	   return obj.orElseThrow(() -> new ObjectNotFoundException(
 			   "Objeto não encontrado! Id:" + id + ", Tipo:" + Categoria.class));
 	}
+	
+	public Categoria insert(Categoria categoria) {
+		categoria.setId(null);
+		return repo.save(categoria);
+	}
 }
