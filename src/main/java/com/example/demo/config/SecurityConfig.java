acc85a7018562eb417f7e0b9ca30bb3397bd31ad
Rodrigo Encine <rodrigoencine@hotmail.com>
2020-05-21 17:17:59 -0300

    configuração Security

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,7 +1,12 @@
 package com.example.demo.config;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -14,17 +19,30 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
+	@Autowired
+	private Environment env;
+	
 	private static final String[] PUBLIC_MATCHES = {
-		"/h2-console/**",
+		"/h2-console/**"
+	};
+	
+
+	private static final String[] PUBLIC_MATCHES_GET = {
 		"/produtos/**",
 		"/categorias/**"
 	};
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		if(Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable();
+		}
+				
+		
 		http.cors().and().csrf().disable();
 		http.authorizeRequests()
 			.antMatchers(PUBLIC_MATCHES).permitAll()
+			.antMatchers(HttpMethod.GET,PUBLIC_MATCHES_GET).permitAll()
 			.anyRequest().authenticated();
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 			
