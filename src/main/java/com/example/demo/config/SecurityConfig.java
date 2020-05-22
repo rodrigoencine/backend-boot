d8e6797758f993feead162f27b189c50272e4420
Rodrigo Encine <rodrigoencine@hotmail.com>
2020-05-22 19:38:58 -0300

    Autorizando endpoints especificos

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -24,6 +25,7 @@ import com.example.demo.security.JWTUtil;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Autowired
 	private JWTUtil jwtUtil;
@@ -40,10 +42,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
 	private static final String[] PUBLIC_MATCHES_GET = {
 		"/produtos/**",
-		"/categorias/**",
-		"/clientes/**"
+		"/categorias/**"
 	};
 	
+	private static final String[] PUBLIC_MATCHES_POST = {
+			"/produtos/**",
+			"/categorias/**",
+			"/clientes"
+		};
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		//super.configure(http);
@@ -55,6 +61,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		http.cors().and().csrf().disable();
 		http.authorizeRequests()
 			.antMatchers(HttpMethod.GET,PUBLIC_MATCHES_GET).permitAll()
+			.antMatchers(HttpMethod.GET,PUBLIC_MATCHES_POST).permitAll()
 			.antMatchers(PUBLIC_MATCHES).permitAll()
 			.anyRequest().authenticated();
 	    http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
